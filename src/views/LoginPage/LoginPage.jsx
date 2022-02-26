a906b70919868d106124327aab3616e90a1a3f14
Sharafat Ibn Mollah Mosharraf <sharafat_8271@yahoo.co.uk>
2022-02-26 08:25:14 +0600

    feat: submit login form on pressing enter

--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -13,6 +13,7 @@ export default class LoginPage extends React.Component {
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleInputKeypress = this.handleInputKeypress.bind(this);
         this.login = this.login.bind(this);
     }
 
@@ -20,6 +21,12 @@ export default class LoginPage extends React.Component {
         const {id, value} = e.target;
 
         this.setState(() => ({[id]: value}));
+    }
+
+    handleInputKeypress(e) {
+        if (e.keyCode === 13) {
+            this.login();
+        }
     }
 
     login() {
@@ -60,9 +67,11 @@ export default class LoginPage extends React.Component {
         return (
             <div className="w-full h-screen">
                 <h1 className="mt-20 text-3xl font-bold">Plagiarism Detector</h1>
-                <div className="rounded-md absolute bg-white lg:w-1/5 lg:-translate-x-1/2 lg:left-1/2 -translate-y-1/2 top-1/2 w-4/5 left-10">
+                <div
+                    className="rounded-md absolute bg-white lg:w-1/5 lg:-translate-x-1/2 lg:left-1/2 -translate-y-1/2 top-1/2 w-4/5 left-10">
                     <div className="flex justify-center -mt-10">
-                        <img className="border-2 bg-white w-20 h-20 rounded-full" src="/logo.png" alt="Logo" width="50" height="50"/>
+                        <img className="border-2 bg-white w-20 h-20 rounded-full" src="/logo.png" alt="Logo" width="50"
+                             height="50"/>
                     </div>
                     <div className="px-12 py-10">
                         <div className="w-full mb-2">
@@ -78,6 +87,7 @@ export default class LoginPage extends React.Component {
                                 <i className="ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock"/>
                                 <input type="password" id="password" placeholder="Password [demo]"
                                        className="-mx-6 px-8 w-full border rounded px-3 py-1 text-gray-700"
+                                       onKeyDown={e => this.handleInputKeypress(e)}
                                        onChange={e => this.handleInputChange(e)} value={this.state.password}/>
                             </div>
                         </div>
